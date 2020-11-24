b4d01a6d7656ee3eb8861ef77213f9fabef73e81
duda <mariaeduardabicalho@gmail.com>
2020-11-24 14:32:29 -0300

    debug urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_tasks,'show_tasks'),
-    path('', views.create_tasks,'create_tasks'),
+    path('', views.show_tasks,'index'),
+   
 ]
